206e1c3047ad4a68a9e82830762c36e779bc8a8f
msattdev <mattsattdev@gmail.com>
2026-01-16 20:30:06 -0500

    qrcode generator init

---
+++ scanmemaybe.py
@@ -0,0 +1,19 @@
+import qrcode
+
+def generate_qr_code(data, filename):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+
+if __name__ == "__main__":
+    data = "https://www.example.com"
+    filename = "qrcode.png"
+    generate_qr_code(data, filename)
+    print(f"QR code generated and saved as {filename}")
